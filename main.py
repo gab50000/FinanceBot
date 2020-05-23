045a7cf3c1495fcf6067c2dda53c73c6b4cc6166
Gabriel Kabbe <gabriel.kabbe@mail.de>
2020-05-23 22:29:36 +0200

    Update bot

--- main.py
+++ main.py
@@ -7,19 +7,28 @@ from aiogram.dispatcher.filters.state import State, StatesGroup
 from aiogram.contrib.fsm_storage.files import MemoryStorage
 from aiogram.utils import executor
 
+from db import Database
+
 logger = logging.getLogger("moneybot")
 
 
 class States(StatesGroup):
+    choice = State()
+
+    purchase_choice = State()
     purchase = State()
+    purchase_list = State()
+
+    payment_choice = State()
     payment = State()
     payment_amount = State()
-    choice = State()
+    payment_list = State()
 
 
 with open("TOKEN", "r") as f:
     bot = Bot(f.read().strip())
 dp = Dispatcher(bot, storage=MemoryStorage())
+database = Database("storage.db")
 
 
 @dp.message_handler(text="cancel", state="*")
@@ -30,8 +39,11 @@ async def cancel(message, state):
 
 @dp.message_handler(commands="start")
 async def start(message):
+    markup = types.ReplyKeyboardMarkup()
+    markup.add("Purchase", "Payment")
+
     await States.choice.set()
-    await message.reply("What do you want to do?")
+    await message.reply("What do you want to do?", reply_markup=markup)
 
 
 @dp.message_handler(
@@ -41,22 +53,40 @@ async def purchase(message):
     await message.reply("Purchase")
 
 
-@dp.message_handler(
-    lambda message: "payment" in message.text.lower(), state=States.choice
-)
+@dp.message_handler(text="Payment", state=States.choice)
 async def payment(message):
+    markup = types.ReplyKeyboardMarkup(one_time_keyboard=True)
+    markup.add("List payments", "New payment")
+
+    await States.payment_choice.set()
+    await message.reply("What exactly do you want to do?", reply_markup=markup)
+
+
+@dp.message_handler(text="List payments", state=States.payment_choice)
+async def list_payments(message, state):
+    history = "\n".join(database.payments)
+    await message.reply(f"Showing you the last payments made:\n{history}")
+    await state.finish()
+
+
+@dp.message_handler(text="New payment", state=States.payment_choice)
+async def new_payment(message):
     await States.payment_amount.set()
     await message.reply("How much do you want to pay?")
 
 
 @dp.message_handler(regexp=r"\d+(\.\d{2})?", state=States.payment_amount)
-async def payment_amount(message):
-    await message.reply(f"You want to pay {message.text}")
+async def payment_amount(message, state):
+    user = message.from_user.mention
+    await message.reply(f"{user} wants to pay {message.text}")
+    await state.finish()
 
 
 @dp.message_handler(regexp=".*", state=States.payment_amount)
 async def invalid_payment_amount(message):
-    await message.reply(f"Invalid")
+    await message.reply(f"Input must be a valid amount of money..")
+    await States.payment_amount.set()
+    await message.reply("How much do you want to pay?")
 
 
 if __name__ == "__main__":
